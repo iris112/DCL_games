34042f4295e4ef4c7522ec45bfe7fc702a97ae02
iris112 <nglayi99@gmail.com>
2020-01-19 09:59:00 -0500

    fix config keys error

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // keys.js - figure out which set of credentials to return
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'dev') {
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
